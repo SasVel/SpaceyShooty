790a144acc4a78fa4b4bc87bfc819436454d25b2
Sas <sasvelchev@gmail.com>
2023-04-28 14:54:57 +0300

    init

---
+++ Player/Player.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+
+@export var SPEED = 500;
+@export var MAX_SPEED = 1500;
+@export var ACCELERATION = 60;
+@export var FRICTION = 30;
+var CURR_SPEED = SPEED
+
+@onready var input_direction = Vector2.ZERO
+@onready var input_vector = Vector2.ZERO
+
+
+func _physics_process(delta):
+	input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	if input_direction != Vector2.ZERO:
+		if input_direction.y < 0:
+			input_vector = input_direction * ((SPEED * 0.6) * delta) * 10
+		elif input_direction.y > 0:
+			input_vector = input_direction * ((SPEED * 1.3) * delta) * 10
+		else:
+			input_vector = input_direction * (SPEED * delta) * 10
+		velocity = input_vector
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, (FRICTION * delta) * 8)
+		
+	move_and_slide()
