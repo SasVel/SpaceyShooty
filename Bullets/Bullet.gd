a9b9f7d668ab4a192ef3860ffbd81e3a3bc32ee9
SasVel <sasvelchev@gmail.com>
2023-05-02 20:14:09 +0300

    Added effects

--- Bullets/Bullet.gd
+++ Bullets/Bullet.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 @export var bullet_speed = 260
+@onready var DeathEffect = preload("res://Effects/ShootPop/ShootPop.tscn")
+
 func _process(delta):
 	position.y -= bullet_speed * delta
 	
@@ -8,4 +10,10 @@ func _process(delta):
 		queue_free()
 
 func _on_hit_box_area_entered(_area):
+	death_effect()
 	queue_free()
+
+func death_effect():
+	var deathEffect = DeathEffect.instantiate()
+	deathEffect.global_position = self.global_position + Vector2(0, 2)
+	get_tree().get_root().add_child(deathEffect)
