c1fe7127b8a3ff9822806fe7a6f017865e6f09b7
Sas <sasvelchev@gmail.com>
2023-05-02 14:32:41 +0300

    Asteroid spawner fix

--- Misc Scripts/Spawner.gd
+++ Misc Scripts/Spawner.gd
@@ -1,16 +1,16 @@
 extends Node2D
 
-@onready var Enemy = preload("res://Enemies/Enemy.tscn")
+@export var Entity : Resource
 @onready var rng = RandomNumberGenerator.new()
 @onready var spawnTimer = $SpawnTimer
 @onready var spawnRateIncreaseTimer = $SpawnRateIncreaseTimer
 
 func _on_timer_timeout():
-	var newEnemy = Enemy.instantiate()
+	var newEntity = Entity.instantiate()
 	var randPos = rng.randi_range(0, 320)
-	newEnemy.position = Vector2(randPos, -10)
-	newEnemy.global_rotation = 3.141
-	get_tree().get_root().add_child(newEnemy)
+	newEntity.position = Vector2(randPos, -10)
+	newEntity.global_rotation = 3.141
+	get_tree().get_root().add_child(newEntity)
 
 
 func _on_spawn_rate_increase_timer_timeout():
--- World/Asteroids/Asteroid.gd
+++ World/Asteroids/Asteroid.gd
@@ -6,11 +6,11 @@ extends CharacterBody2D
 @onready var direction = Vector2(0, 1)
 
 @onready var stats = $Stats
-@onready var DeathEffect = preload("res://Effects/Explosion_1/Explosion_1.tscn")
+@export var DeathEffect: Resource
 @onready var explosionCollision = $ExplosionHitBox
 
 func _physics_process(delta):
-	#velocity = direction * (speed * delta) * 10
+	velocity = direction * (speed * delta) * 10
 	self.rotation += -0.01
 	move_and_slide()
 	
@@ -18,9 +18,15 @@ func _on_hurt_box_area_entered(area):
 	stats.health -= area.damage
 
 func _on_stats_no_health():
-#	var entitiesColliding = explosionCollision.get_overlapping_bodies()
-#	for e in entitiesColliding:
-#		e.get_child(0).health -= explosionStrength
+	var entitiesColliding = explosionCollision.get_overlapping_bodies()
+	for e in entitiesColliding:
+		if e.get_node_or_null("Stats") != null:
+			e.get_node("Stats").health -= explosionStrength
+		elif e.get_node_or_null("PlayerStats") != null:
+			PlayerStats.health -= explosionStrength
+		else:
+			e.queue_free()
+			
 	create_death_explosion()
 	queue_free()
 
